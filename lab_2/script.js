a533ada58abb584efcb52d6da0bdf8561df1ae4c
dianalubentsova2002 <62855415+dianalubentsova2002@users.noreply.github.com>
2024-05-22 01:21:09 +0300

    lab 2

---
+++ lab_2/script.js
@@ -0,0 +1,24 @@
+const transliterationMap = {
+    'а': 'a', 'б': 'b', 'в': 'v', 'г': 'h', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ie', 'ж': 'zh', 'з': 'z',
+    'и': 'y', 'і': 'i', 'ї': 'i', 'й': 'i', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+    'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
+    'ь': "'", 'ю': 'iu', 'я': 'ia', 'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'H', 'Ґ': 'G', 'Д': 'D', 'Е': 'E',
+    'Є': 'Ye', 'Ж': 'Zh', 'З': 'Z', 'И': 'Y', 'І': 'I', 'Ї': 'Yi', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M',
+    'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts',
+    'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ь': "'", 'Ю': 'Yu', 'Я': 'Ya'
+};
+
+function transliterateUkrainian(text) {
+    return text.split('').map(char => {
+        return transliterationMap[char] || char;
+    }).join('');
+}
+
+const ukrainianTextarea = document.getElementById('ukrainian-text');
+const transliteratedTextarea = document.getElementById('transliterated-text');
+
+ukrainianTextarea.addEventListener('input', () => {
+    const ukrainianText = ukrainianTextarea.value;
+    const transliteratedText = transliterateUkrainian(ukrainianText);
+    transliteratedTextarea.value = transliteratedText;
+});
